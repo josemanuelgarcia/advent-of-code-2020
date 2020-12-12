1a67d14bd2177d54ea3dae07cfab8bdc26728c1a
josemanuelgarcia <josececos@gmail.com>
2020-12-12 19:54:37 +0100

    add day 4

--- src/main/scala/com/jmgf/adventofcode/Day4.scala
+++ src/main/scala/com/jmgf/adventofcode/Day4.scala
@@ -6,7 +6,7 @@ import scala.util.Try
 
 object Day4 extends Puzzle[Seq[String], Int, Int] {
 
-  val obligatoryFields = Seq("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+  private val obligatoryFields = Seq("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
   override def parse(resource: String): Seq[String] =
     File.readResource(resource).flatMap(row => Try(row.toString).toOption)
@@ -22,7 +22,21 @@ object Day4 extends Puzzle[Seq[String], Int, Int] {
 
   override def part2(inputs: Seq[String]): Int = {
     val inputProcessed = processInput(inputs)
-    0
+
+    val lista = inputProcessed.map(_.split(" ").map(t => {
+      val a = t.split(":")
+      a(0) -> a(1)
+    }).toMap)
+
+    lista
+      .filter(t => obligatoryFields.forall(t.contains(_)))
+      .count(t => isByrCorrect(t("byr"))
+        && isIyrCorrect(t("iyr"))
+        && isEyrCorrect(t("eyr"))
+        && isHgtCorrect(t("hgt"))
+        && isHclCorrect(t("hcl"))
+        && isEclCorrect(t("ecl"))
+        && isPidCorrect(t("pid")))
   }
 
   def processInput(inputs: Seq[String]): List[String] = {
@@ -40,5 +54,22 @@ object Day4 extends Puzzle[Seq[String], Int, Int] {
     }
   }
 
+  val isByrCorrect: String => Boolean = (valor: String) => {1920 <= valor.toInt  && valor.toInt <= 2002}
+  val isIyrCorrect: String => Boolean = (valor: String) => {2010 <= valor.toInt  && valor.toInt <= 2020}
+  val isEyrCorrect: String => Boolean = (valor: String) => {2020 <= valor.toInt  && valor.toInt <= 2030}
+
+  val isHgtCorrect: String => Boolean = (valor: String) => {
+    val regex = """(\d+(cm|in)+)""".r
+    regex.findFirstMatchIn(valor).isDefined && (
+      (valor.takeRight(2) == "cm" && 150 <= valor.dropRight(2).toInt && valor.dropRight(2).toInt <= 193) ||
+        (valor.takeRight(2) == "in" && 59 <= valor.dropRight(2).toInt && valor.dropRight(2).toInt <= 76))
+  }
+  val isHclCorrect: String => Boolean = (valor: String) => {
+    val regex = """(#([a-z0-9]{6}))""".r
+    valor.length == 7 && regex.findFirstMatchIn(valor).isDefined
+  }
+
+  val isEclCorrect: String => Boolean = (valor: String) => {Seq("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(valor)}
+  val isPidCorrect: String => Boolean = (valor: String) => {valor.length == 9 && valor.forall(Character.isDigit)}
 
 }
