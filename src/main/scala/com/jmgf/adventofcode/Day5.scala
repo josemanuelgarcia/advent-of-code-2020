8677588513b7dab6a27998ab3e1f15156bb99651
josemanuelgarcia <josececos@gmail.com>
2020-12-13 01:00:05 +0100

    day 5 part 2

--- src/main/scala/com/jmgf/adventofcode/Day5.scala
+++ src/main/scala/com/jmgf/adventofcode/Day5.scala
@@ -13,14 +13,23 @@ object Day5 extends Puzzle[Seq[Array[Char]], Int, Int] {
     File.readResource(resource).flatMap(row => Try(row.toCharArray).toOption)
 
   override def part1(inputs: Seq[Array[Char]]): Int = {
+    getAllIds(inputs).max
+  }
+
+  override def part2(inputs: Seq[Array[Char]]): Int = {
+    val ids: List[Int] = getAllIds(inputs)
+
+    (ids.min to ids.max).filterNot(ids.contains(_)).head
+
+  }
+
+  private def getAllIds(inputs: Seq[Array[Char]]): List[Int] = {
     inputs.map(input => {
       val row = calculatePosition(input.dropRight(3), 0, 127)
       val column = calculatePosition(input.takeRight(3), 0, 7)
       row * 8 + column
-    }).toList.max
+    }).toList
   }
-
-  override def part2(inputs: Seq[Array[Char]]): Int = 0
 
   private def calculatePosition(arrayCharacters: Array[Char], lowRange: Int, highRange: Int): Int = {
    if(arrayCharacters.length == 1) {
