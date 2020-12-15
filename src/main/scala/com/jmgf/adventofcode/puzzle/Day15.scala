910b66158aec19d946453dbd28aca9655477b572
josemanuelgarcia <josececos@gmail.com>
2020-12-15 15:32:46 +0100

    part 1 day 15

---
+++ src/main/scala/com/jmgf/adventofcode/puzzle/Day15.scala
@@ -0,0 +1,41 @@
+package com.jmgf.adventofcode.puzzle
+
+import com.jmgf.adventofcode.Puzzle
+import com.jmgf.adventofcode.util.File
+
+import scala.util.Try
+
+object Day15 extends Puzzle[Seq[Int], Int, Int] {
+
+  override def parse(resource: String): Seq[Int] =
+    File.readResource(resource).flatMap(row => Try(row.toInt).toOption)
+
+
+  override def part1(inputs: Seq[Int]): Int = {
+    var gameSeq = inputs
+
+    while(gameSeq.length < 2020) {
+      val lastNumber = gameSeq.last
+      if (gameSeq.count(_ == lastNumber) == 1) gameSeq = gameSeq :+ 0
+      else {
+        val a = gameSeq.dropRight(1).indices.filter(pos => gameSeq(pos) == lastNumber).max
+        gameSeq = gameSeq :+ (gameSeq.length - (a + 1))
+      }
+    }
+    gameSeq.last
+  }
+
+  override def part2(inputs: Seq[Int]): Int = {
+    var gameSeq = inputs
+
+    while(gameSeq.length < 30000000) {
+      val lastNumber = gameSeq.last
+      if (gameSeq.count(_ == lastNumber) == 1) gameSeq = gameSeq :+ 0
+      else {
+        val a = gameSeq.dropRight(1).indices.filter(pos => gameSeq(pos) == lastNumber).max
+        gameSeq = gameSeq :+ (gameSeq.length - (a + 1))
+      }
+    }
+    gameSeq.last
+  }
+}
