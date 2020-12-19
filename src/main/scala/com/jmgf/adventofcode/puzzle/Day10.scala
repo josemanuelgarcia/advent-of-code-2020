7b28d0319820630ff45ef4a09b3754a021dd9b8a
josemanuelgarcia <josececos@gmail.com>
2020-12-19 18:57:26 +0100

    day 1o part 2

--- src/main/scala/com/jmgf/adventofcode/puzzle/Day10.scala
+++ src/main/scala/com/jmgf/adventofcode/puzzle/Day10.scala
@@ -8,7 +8,7 @@ import scala.util.Try
 /**
   * Day 10 puzzle
   */
-object Day10 extends Puzzle[Seq[Int], Int, Int] {
+object Day10 extends Puzzle[Seq[Int], Int, BigInt] {
 
   override def parse(resource: String): Seq[Int] =
     File.readResource(resource).flatMap(row => Try(row.toInt).toOption)
@@ -19,21 +19,22 @@ object Day10 extends Puzzle[Seq[Int], Int, Int] {
    joltDifferences.count(_.equals(1)) * joltDifferences.count(_.equals(3))
   }
 
-  override def part2(inputs: Seq[Int]): Int = {
-    val sorted = inputs.sorted :+ (inputs.max + 3)
-    val allCombinations = getAllJoltCombinations(sorted, Seq(sorted.head))
+  override def part2(inputs: Seq[Int]): BigInt = {
+    val joltsSorted = 0 +: inputs.sorted :+ (inputs.max + 3)
+    val joltDifferences = joltsSorted.zip(joltsSorted.drop(1)).map({case(a, b) => b - a})
 
-    allCombinations.length
+    val possible_permutations = Seq(1, 1, 2, 4, 7)
+    var acc: Long = 1
+    var last_range = 0
+    joltDifferences.foreach(i => {
+      if (i == 1) {
+        last_range += 1
+      } else if (i == 3) {
+        acc *= possible_permutations(last_range)
+        last_range = 0
+      }
+    })
+    acc
   }
 
-
-  private def getAllJoltCombinations(joltAdapters: Seq[Int], currentCombination: Seq[Int]): Seq[Seq[Int]] = {
-    val paths: Seq[Seq[Int]] = Seq()
-    if(currentCombination.last.equals(joltAdapters.max)) paths :+ currentCombination
-    else {
-      val posibilities: Seq[Int] = joltAdapters.filter(_ <= currentCombination.last + 3)
-      posibilities.foreach(a => paths :+ getAllJoltCombinations(joltAdapters, currentCombination :+ a))
-      paths
-    }
-  }
 }
